3876aa09b6b35fbe0f0b90d4b12c04eeaa4274ae
Jocelyn GENNESSEAUX <jocelyn@gennesseaux.fr>
2022-11-15 08:52:41 +0100

    added html and plain body to client

--- client/client.go
+++ client/client.go
@@ -16,10 +16,12 @@ type Mailer struct {
 }
 
 type Message struct {
-	From    string `json:"from"`
-	To      string `json:"to" example:"me@gmail.com,you@gmail.com"`
-	Subject string `json:"subject"`
-	Body    string `json:"body"`
+	From      string `json:"from,omitempty"`
+	To        string `json:"to" example:"me@gmail.com,you@gmail.com"`
+	Subject   string `json:"subject"`
+	Body      string `json:"body,omitempty"`
+	HtmlBody  string `json:"html_body,omitempty" example:"Hello!"`
+	PlainBody string `json:"plain_body,omitempty" example:"<p>Hello!</p>"`
 }
 
 type Authentication struct {
